442c8284629cb06de6502e728bc19b0c6582c6ac
Ahmad <achehre@yahoo.com>
2017-07-18 04:10:33 +0430

    Project structured.

---
+++ tests/EndToEndTests/ZeroMq/Tests.ZeroMq.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tests.ZeroMq.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
