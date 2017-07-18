17a87d3bda82e9ee5ccf4477ac51251ddb343789
Ahmad <achehre@yahoo.com>
2017-07-18 23:06:52 +0430

    CustomerService added to response the messages.

---
+++ tests/EndToEndTests/ZeroMq/Tests.ZeroMq.ConsoleApp/CustomerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Messaging.Infrastructure.Messaging;
+using Messaging.Infrastructure.Messaging.ZeroMq;
+using Tests.ZeroMq.CommandQuery;
+
+namespace Tests.ZeroMq.ConsoleApp
+{
+    public class CustomerService
+    {
+        public void CreateCustomer(IMessageQueue<MessageQueueConfig> messageQueue, Message message)
+        {
+
+            var customerName = message.BodyAs<CreateCustomerRequest>().Name;
+            // Create Customer 
+            var customerCreatedResponse = new CustomerCreatedResponse()
+            {
+                Id = 10  //Created customer Id
+            };
+
+
+            var replyQueue = messageQueue.GetReplyQueue(message);
+
+            replyQueue.Send(new Message()
+            {
+                Body = customerCreatedResponse,
+                ResponseAddress = message.ResponseAddress
+
+            });
+
+        }
+
+    }
+}
--- tests/EndToEndTests/ZeroMq/Tests.ZeroMq.ConsoleApp/Program.cs
+++ tests/EndToEndTests/ZeroMq/Tests.ZeroMq.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using Messaging.Infrastructure.Messaging;
+using Messaging.Infrastructure.Messaging.ZeroMq;
+using Tests.ZeroMq.CommandQuery;
 
 namespace Tests.ZeroMq.ConsoleApp
 {
@@ -7,6 +11,22 @@ namespace Tests.ZeroMq.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+
+        private static void Listen(string name, MessagePattern pattern)
+        {
+            var factory = new ZeroMqMessageQueueFactory();
+            var config = new ZeroMqMessageQueueConfig
+            {
+                MessageQueueName = "CreateCustomer",
+                MessagePattern = MessagePattern.RequestResponse
+            };
+            var queue = factory.CreateInboundQueue(config);
+            //queue.Listen(q =>
+            //{
+            //    (new CustomerService()).CreateCustomer(queue, q);
+            //});
         }
     }
 }
