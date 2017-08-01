fb3a1f6c07e727ca274e7fd506c6b6d067413895
Ahmad <achehre@yahoo.com>
2017-08-01 17:15:43 +0430

    RabbitMQ test project added.

---
+++ tests/EndToEndTests/RabbitMq/SimpleTest/SimpleTest.Client/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace SimpleTest.Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ScreenTop("Client");
+
+
+            var model = CreateModel();
+            //var queueName = "queuetestapp01";
+            var exchangeName = "exchangetestapp01";
+
+            //CreateExchangeAndQueue(model, queueName, exchangeName);
+
+            var basicProperties = model.CreateBasicProperties();
+            basicProperties.Persistent = false;
+
+            var message = Encoding.UTF8.GetBytes("Hello 3 from app!");
+
+            var key = "testapp01";
+            model.BasicPublish(exchangeName, key, basicProperties, message);
+            Show($"Message {Encoding.UTF8.GetString(message)} published.");
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private static void CreateExchangeAndQueue(IModel model, string queueName, string exchangeName)
+        {
+            model.QueueDeclare(queueName, true, false, false, null);
+
+            Show($"Queue {queueName} Created.");
+
+            model.ExchangeDeclare(exchangeName, ExchangeType.Topic, true, false, null);
+
+            Show($"Exchange {exchangeName} Created.");
+
+            model.QueueBind(queueName, exchangeName, "testapp01");
+
+            Show($"Queue {queueName} and exchange {exchangeName} binded.");
+        }
+
+        private static IModel CreateModel()
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+
+
+            var connection = factory.CreateConnection();
+            Show("Connection Created.");
+            var model = connection.CreateModel();
+            Show("Model Created;");
+            return model;
+        }
+
+        private static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void ScreenTop(string title)
+        {
+            var dashes = new string('-', title.Length + 20);
+
+            Console.WriteLine(dashes);
+            Console.WriteLine($"|{new string(' ', 9)}{title}{new string(' ', 9)}|");
+            Console.WriteLine(dashes);
+        }
+    }
+}
---
+++ tests/EndToEndTests/RabbitMq/SimpleTest/SimpleTest.Server/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace SimpleTest.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queueName = "queuetestapp01";
+            //var exchangeName = "exchangetestapp01";
+
+
+            ScreenTop("Server");
+            var model = CreateModel();
+
+            var consumer = new QueueingBasicConsumer(model);
+            model.BasicConsume(queueName, false, consumer);
+
+            while (true)
+            {
+                //Get next message
+                var deliveryArgs = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                //Serialize message
+                var message = Encoding.Unicode.GetString(deliveryArgs.Body);
+
+                Console.WriteLine("Message Recieved - {0}", message);
+                model.BasicAck(deliveryArgs.DeliveryTag, false);
+            }
+
+
+        }
+
+
+        private static IModel CreateModel()
+        {
+            var factory = new ConnectionFactory
+            {
+                UserName = "guest",
+                Password = "guest",
+                HostName = "localhost"
+            };
+
+
+            var connection = factory.CreateConnection();
+            Show("Connection Created.");
+            var model = connection.CreateModel();
+            Show("Model Created;");
+            model.BasicQos(0, 1, false);
+            return model;
+        }
+
+
+        private static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+        private static void ScreenTop(string title)
+        {
+            var dashes = new string('-', title.Length + 20);
+
+            Console.WriteLine(dashes);
+            Console.WriteLine($"|{new string(' ', 9)}{title}{new string(' ', 9)}|");
+            Console.WriteLine(dashes);
+        }
+    }
+}
