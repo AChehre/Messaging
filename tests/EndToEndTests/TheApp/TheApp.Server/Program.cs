dbae1923bf949e618d17d17f04d4d579024a6d1b
Ahmad <achehre@yahoo.com>
2017-08-09 13:29:19 +0430

    TheApp projects added.

---
+++ tests/EndToEndTests/TheApp/TheApp.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheApp.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
